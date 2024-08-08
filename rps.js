ddb63d59bda12e6ac5e112fed8761b83e8287300
VedantPV <86618229+VedantPV@users.noreply.github.com>
2024-08-08 14:43:42 -0400

    Completion of this Project:
    This serves as a reminder to myself to constantly commit,
    seeing as I forgot to and this will be the only commit message
    in the log
    
    The two main problems I encountered in this Project
    had to do with boolean conditions and what to return in
    playRound()
    
    First, understanding the differences between == and = when
    comparing equivalences between two objects or variables
    
    Second, whether to return boolean values for the winner
    var or string values
    Of the two, I decided on String characters as it allowed for multiple
    return and if conditonal statements to decide the score increments
    between both scoring variables
    
    While this wasn't quite as hard as I expected, it was my First
    time properly coding in JS and from here on I will try to pay attention
    to certain syntax and what to return
    
    Also, I will force myself to commit more

---
+++ rps.js
@@ -0,0 +1,115 @@
+console.log("Hello World");
+console.log(Math.floor(Math.random()*3));
+//0: rock, 1: scissors, 2: paper
+
+
+function getComputerChoice() {
+    return Math.floor(Math.random()*3);
+}
+
+function getHumanChoice() {
+    let choice = prompt("Enter your choice: Rock, Paper, or Scissors?");
+    choice = choice.toLowerCase();
+    switch(choice){
+        case("rock"): return 0;
+        case("scissors"): return 1;
+        case("paper"): return 2;
+    }
+
+}
+
+function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()) {
+    console.log(humanChoice);
+    console.log(computerChoice);
+  
+        if(humanChoice == 0)
+        {
+            if(computerChoice == humanChoice)
+            {
+                console.log("Tie!");
+                return "T";
+            }
+            else if(computerChoice == 1)
+            {
+                console.log("You Win! Rock Beats Scissors");
+                return "H";
+               
+            }
+            else
+            {
+                console.log("You Lose! Paper Beats Rock");
+                return "C";
+            }
+        }
+
+        else if(humanChoice == 1)
+        {
+            if(computerChoice == 0)
+            {
+                console.log("You Lose! Rock Beats Scissors");
+                return "C";
+               
+            }    
+            else if(computerChoice == humanChoice)
+            {
+                console.log("Tie!");
+                return "T";
+            }
+            else
+            {
+                console.log("You Win! Scissors Beats Paper");
+                return "H";
+            }
+        }
+
+        else if(humanChoice == 2)
+        {    
+            if(computerChoice == 0)
+            {
+                console.log("You Win! Paper Beats Rock");
+                return "H";
+               
+            }
+            else if(computerChoice == 1)
+            {
+                console.log("You Lose! Scissors Beats Paper");
+                return "C";
+             
+            }
+            else
+            {
+                console.log("Tie!")
+                return "T";
+            }
+        }
+
+}
+
+function playGame()
+{
+    let humanScore = 0;
+    let computerScore = 0;
+    for(let x = 0; x < 5; x++)
+    {
+        let winner = playRound();
+    
+        if(winner == "H")
+        {
+            humanScore++;
+        }
+        else if(winner == "C")
+        {
+            computerScore++;
+        }
+        console.log("Human Score: "+humanScore+"\nComputer Score: "+computerScore);
+    }
+    if(humanScore > computerScore)
+    {
+        console.log("You Win! Congrats!")
+    }
+    else
+    {
+        console.log("You Lost! Try Again!");
+    }
+
+}
